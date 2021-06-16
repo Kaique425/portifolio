6b9c4371a211713c59836bcc0359991c46a56c89
Kaique <motamendes.kaique.100@gmail.com>
2021-06-16 19:04:45 -0300

    Deploy

--- portifolio/settings/base.py
+++ portifolio/settings/base.py
@@ -124,8 +124,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
-
+]   
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- portifolio/urls.py
+++ portifolio/urls.py
@@ -1,4 +1,3 @@
-from portifolio.settings import DEBUG
 from django.contrib import admin
 from django.urls import include, path
 
