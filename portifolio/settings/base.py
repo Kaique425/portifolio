fc4445ff748ebe97a63d489a5b35b939e1a24229
Kaique <motamendes.kaique.100@gmail.com>
2021-06-16 21:00:44 -0300

    make it better

--- portifolio/settings/base.py
+++ portifolio/settings/base.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for portifolio project.
 
@@ -120,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
