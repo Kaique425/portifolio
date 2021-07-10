a269ab457387778177508535e12d4d617f48b6e1
Kaique <motamendes.kaique.100@gmail.com>
2021-07-09 22:49:32 -0300

    .

--- portifolio/settings/base.py
+++ portifolio/settings/base.py
@@ -135,5 +135,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
