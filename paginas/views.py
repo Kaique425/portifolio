9b71dba2f558c47742be697bc7bb1f85fe421b67
Kaique <motamendes.kaique.100@gmail.com>
2021-08-19 20:37:43 -0300

    Adding autoslugfield to requirements.txt

--- paginas/views.py
+++ paginas/views.py
@@ -5,10 +5,11 @@ def home(request):
     url = request.get_full_path()
     projetos = Projeto.objects.all()
     tecnologias = Tecnologias.objects.all()
-    context = {'projetos':projetos, 'tecnologias':tecnologias}
-    return render(request,'paginas/home.html', context)
+    context = {'projetos': projetos, 'tecnologias': tecnologias}
+    return render(request, 'paginas/home.html', context)
+
 
 def projeto(request, slug):
     projetos = Projeto.objects.get(slug=slug)
-    context = {'projetos':projetos}
-    return render(request,'paginas/projeto.html', context)
+    context = {'projetos': projetos}
+    return render(request, 'paginas/projeto.html', context)
